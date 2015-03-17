7c7c085c5d327ffc0c2b3fc3b6547d1fd47427c6
Sergio Gonzalez <alu0100354256@ull.edu.es>
2015-03-17 22:17:36 +0000

    Test mocha y chai

---
+++ test/tests.js
@@ -0,0 +1,35 @@
+var assert = chai.assert;
+
+
+suite('CSV', function() {
+    setup(function(){
+      if (typeof __html__ !== 'undefined') {
+        document.body.innerHTML = __html__['tests/index.html'];
+        original = document.getElementById('original');
+        finaltable = document.getElementById('finaltable');
+      }  
+    });
+
+   test('Local Storage', function () {
+     original.value='Local Storage Test';
+     calculate();
+
+     assert.deepEqual(localStorage.original, 'Local Storage Test');
+   });
+
+   test('Tabla Correcta', function () {
+     original.value = 'a, "b"\nc, d';
+     calculate();
+
+     var esperado =  '\n        <table class="center" id="result">\n            \n                    <tbody><tr>\n                    \n                    <td>a</td>\n                    \n                    <td>b</td>\n                    \n                </tr>\n            \n                    <tr>\n                    \n                    <td>c</td>\n                    \n                    <td> d</td>\n                    \n                </tr>\n            \n        </tbody></table>\n    ';
+     assert.deepEqual(finaltable.innerHTML, esperado);
+   });
+
+   test('Tabla Incorrecta', function () {
+     original.value = 'a, b, c\nd, e';
+     calculate();
+
+     var valueExpected = '\n        <table class="center" id="result">\n            \n                    <tbody><tr>\n                    \n                    <td>a</td>\n                    \n                    <td> b</td>\n                    \n                    <td> c</td>\n                    \n                </tr>\n            \n                    <tr class="error">\n                    \n                    <td>d</td>\n                    \n                    <td> e</td>\n                    \n                </tr>\n            \n        </tbody></table>\n    ';
+     assert.deepEqual(finaltable.innerHTML, valueExpected);
+   });
+});
