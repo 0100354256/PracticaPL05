8cce1222c8019f7e3a1b77d6ddb6cf5d91d9e799
Sergio Gonzalez <alu0100354256@ull.edu.es>
2015-03-18 11:57:20 +0000

    Correccion

--- conf.js
+++ conf.js
@@ -39,6 +39,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'index.html': ['html2js'],
+      'test/index.html': ['html2js']
     },
 
 
