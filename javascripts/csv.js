1f780fb640a1c36132e52f553112e95635dd45ea
Sergio Gonzalez <alu0100354256@ull.edu.es>
2015-03-18 19:41:19 +0000

    Test

--- javascripts/csv.js
+++ javascripts/csv.js
@@ -1,5 +1,6 @@
 exports.button = function(cadena) {
   var original = cadena.original;
+  console.log(original);
   var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
   var lines = original.split(/\n+\s*/);
   var commonLength = NaN;
