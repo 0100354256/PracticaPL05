77e373b1f7f91daeaddd25f820944cd829b0d06a
Sergio Gonzalez <alu0100354256@ull.edu.es>
2015-03-18 19:47:15 +0000

    Test

--- public/conf.js
+++ public/conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['requirejs', 'mocha', 'chai', 'jasmine'],
+    frameworks: ['requirejs', 'exports', 'mocha', 'chai', 'jasmine'],
 
 
     // list of files / patterns to load in the browser
