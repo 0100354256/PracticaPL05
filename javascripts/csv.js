c4b697662138be698729254fb9293ba0d32ed594
Sergio Gonzalez <alu0100354256@ull.edu.es>
2015-03-18 18:52:55 +0000

    Test

--- javascripts/csv.js
+++ javascripts/csv.js
@@ -3,6 +3,7 @@ if(typeof exports == 'undefined'){
 }
 exports.button = function(cadena) {
   var original = cadena.original;
+  debug.log("+++++++++++*" + original);
   var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
   var lines = original.split(/\n+\s*/);
   var commonLength = NaN;
